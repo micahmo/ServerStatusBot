3a6f4e468b23b9ede2e2463d8c3766b4080740e0
Micah Morrison <micahmorrison95@gmail.com>
2020-08-25 09:09:00 -0400

    First commit

---
+++ ServerStatusBot/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using File = System.IO.File;
+
+namespace ServerStatusBot
+{
+    class Program
+    {
+        #region Main method
+
+        static void Main(string[] args)
+        {
+            _startDateTime = DateTimeOffset.UtcNow;
+
+            _botClient = new TelegramBotClient(File.ReadAllLines("token.txt")[0]);
+            _micahmo = Convert.ToInt32(File.ReadAllLines("micahmo.txt")[0]);
+
+            _botClient.OnMessage += Bot_OnMessage;
+            _botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            string messageText = e.Message.Text;
+            ChatId chatId = e.Message.Chat.Id;
+
+            if (messageText == "/status" && chatId.Identifier == _micahmo.Identifier)
+            {
+                var startTimeInEt = TimeZoneInfo.ConvertTime(_startDateTime, GetEasternTimeZone());
+
+                await _botClient.SendTextMessageAsync(
+                    chatId: _micahmo,
+                    text: $"Status is good. Running on host '{Environment.MachineName}' (platform is {GetOsPlatform()}). Uptime is {DateTimeOffset.UtcNow - _startDateTime} " +
+                          $"(since {startTimeInEt})",
+                    parseMode: ParseMode.Html
+                );
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static TimeZoneInfo GetEasternTimeZone()
+        {
+            if (TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(tz => tz.Id == "America/New_York") is TimeZoneInfo linuxTz)
+            {
+                return linuxTz;
+            }
+            else if (TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(tz => tz.Id == "Eastern Standard Time") is TimeZoneInfo windowsTz)
+            {
+                return windowsTz;
+            }
+            else return null;
+        }
+
+        private static string GetOsPlatform()
+        {
+            return (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OSPlatform.Windows.ToString() : RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSPlatform.Linux.ToString() : "Unknown");
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private static DateTimeOffset _startDateTime;
+
+        private static TelegramBotClient _botClient;
+
+        private static ChatId _micahmo;
+
+        #endregion
+    }
+}
---
+++ ServerStatusBot/rebuild_docker_release.ps1
@@ -0,0 +1,20 @@
+# Set the Docker host to the first command-line argument (if any)
+# Example usage: .\rebuild_docker_release.ps1 ssh://user@server
+$env:docker_host = $args[0]
+
+# Rebuild/publish the code
+dotnet publish -c Release
+
+# Stop/delete the existing container, then delete the existing image
+docker stop serverstatusbot-release-container
+docker rm serverstatusbot-release-container
+docker rmi serverstatusbot-release
+
+# Rebuild the image, recreate the container, and start the container
+docker build -t serverstatusbot-release -f Dockerfile_release .
+
+# Use Docker run to create + start the container
+# -d: Detached mode (so that we're not stuck with stdout in tty)
+docker run -d --restart always --name serverstatusbot-release-container serverstatusbot-release
+
+$env:docker_host = ""
